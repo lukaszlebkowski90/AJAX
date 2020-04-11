af2ee1c91b411a933e8adcebf966820b555f472b
Łukasz Łebkowski <lukasz.lebkowski90@gmail.com>
2020-04-11 11:18:34 +0200

    poprzednia wersja

--- scripts.js
+++ scripts.js
@@ -3,15 +3,17 @@ var url = 'http://api.icndb.com/jokes/random';
 var button = document.getElementById('get-joke');
 button.addEventListener('click', function() {
   getJoke();
-this.DOCUMENT_POSITION_DISCONNECTED});
-var paragraph = daocument.getElementById('joke');
-asdadsadsads
-function getJoke()asdad {
+});
+var paragraph = document.getElementById('joke');
+
+function getJoke() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.addEventListener('load', function() {
     var response = JSON.parse(xhr.response);
     paragraph.innerHTML = response.value.joke;
   });
-zmiany na vc code ciag dalszy 
+  xhr.send();
 }
+var response = JSON.parse(xhr.response);
+paragraph.innerHTML = response.value.joke;
